03ed70d0ed0b35fe7cd75f029d642eddcbdc22a9
Gabriel <g.braga@ufms.br>
2021-03-05 22:29:30 -0400

    First commit

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express'
+import { getCustomRepository } from 'typeorm'
+import { UsersRepository } from '../repositories/UsersRepository'
+import * as yup from 'yup'
+import { AppError } from '../errors/AppError'
+
+class UserController{
+    async create(req: Request, res: Response){
+        const {name, email} = req.body
+        const schema = yup.object().shape({
+            name: yup.string().required("Nome obrigatório"),
+            email: yup.string().email().required("Email inválido")
+        })
+        try {
+            await schema.validate(req.body, {abortEarly: false})
+        } catch (err) {
+            throw new AppError(err)
+
+        }
+        const usersRepository = getCustomRepository(UsersRepository)
+        const userAlreadyExists = await usersRepository.findOne({email})
+        if(userAlreadyExists) {
+            throw new AppError("User already exists!")
+        }
+        const user = usersRepository.create({name, email})
+        await usersRepository.save(user)
+        return res.status(201).json(user)
+    }
+    async show(req: Request, res: Response){
+        const userRepository = getCustomRepository(UsersRepository)
+
+        const all = await userRepository.find()
+
+        return res.json(all)
+    }
+}
+
+export { UserController }
